f20c5b1a3f165fcada10b6a5ad82fcf505fac5af
lacriza <lorafordance@gmail.com>
2025-07-10 17:14:02 +0300

    CW-16 Added READ.ME file Added test with random pass and username Added lib faker

--- tests/lesson-16-1.spec.ts
+++ tests/lesson-16-1.spec.ts
@@ -1,9 +1,10 @@
 import { test, Locator, expect} from '@playwright/test';
+import {faker} from "@faker-js/faker/locale/ar";
 
 test.beforeEach(async ({ page }) => {
 
 
-    await page.goto('https://fe-delivery.tallinn-learning.ee/signin');
+    await page.goto(process.env.APP_URL);
 
 })
 
@@ -14,4 +15,34 @@ test('button disabled initially', async ({ page }) => {
     await userName.fill('John');
     await password.fill('test');
     await expect(signInButton).toBeDisabled();
+});
+
+test('Popup appears after sign in clicking when name and pass not correct ENG', async ({ page }) => {
+    const randomUsername = faker.internet.username();
+    const randomPassword = faker.internet.password(8);
+    const signInButton: Locator = page.getByRole("button",{name: 'Sign In'});
+    const popupMessage: Locator = page.locator('[data-name="authorizationError-popup"]');
+
+    await page.fill('#username', randomUsername);
+    await page.fill('#password', randomPassword);
+    await signInButton.click();
+    await expect(popupMessage).toBeVisible();
+    await expect(popupMessage).toHaveText(`×Incorrect credentials`
+    );
+});
+
+test('Popup appears after sign in clicking when name and pass not correct RU', async ({ page }) => {
+    const langRu : Locator = page.getByRole('button', {name: 'RU'});
+    await langRu.click();
+    const randomUsername = faker.internet.username();
+    const randomPassword = faker.internet.password(8);
+    const signInButton: Locator = page.getByRole("button",{name: 'Войти'});
+    const popupMessage: Locator = page.locator('[data-name="authorizationError-popup"]');
+
+    await page.fill('#username', randomUsername);
+    await page.fill('#password', randomPassword);
+    await signInButton.click();
+    await expect(popupMessage).toBeVisible();
+    await expect(popupMessage).toHaveText(`×Неверные учетные данные`
+    );
 });
